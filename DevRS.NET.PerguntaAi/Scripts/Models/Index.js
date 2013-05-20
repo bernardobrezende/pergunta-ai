332509bd485058f75ee0e202580d3c7581398267
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2013-05-20 20:45:18 -0300

    adicionando tratamento para evitar que perguntas repetidas sejam adicionadas.

--- DevRS.NET.PerguntaAi/Scripts/Models/Index.js
+++ DevRS.NET.PerguntaAi/Scripts/Models/Index.js
@@ -3,14 +3,25 @@
 function Index() {
     self = this;
     self.textoPergunta = ko.observable("");
-    self.perguntas = ko.observableArray();
+    self.textoErro     = ko.observable("");
+    self.perguntas     = ko.observableArray();
 
     self.deveExibirBotao = ko.computed(function () {
         return self.textoPergunta().length > 0;
     }, this);
 
+    self.deveExibirErro = ko.computed(function () {
+        return self.textoErro().length > 0;
+    }, this);
+
     self.perguntar = function () {
-        $.connection.perguntasHub.server.perguntar(self.textoPergunta());
+        var perguntaRepetida = ko.utils.arrayFirst(self.perguntas(), function (p) { return p === self.textoPergunta(); });
+        if (perguntaRepetida)
+            self.textoErro("Ops.. Pergunta já foi feita!");
+        else {
+            self.textoErro("");
+            $.connection.perguntasHub.server.perguntar(self.textoPergunta());
+        }
     };
 
     self.adicionarPergunta = function (pergunta) {
