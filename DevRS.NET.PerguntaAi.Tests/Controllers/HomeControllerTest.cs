c3c1c5582fd3c6fd787047b3baa7b70f4b651310
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2013-05-20 12:54:17 -0300

    refatorando testes.

--- DevRS.NET.PerguntaAi.Tests/Controllers/HomeControllerTest.cs
+++ DevRS.NET.PerguntaAi.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using DevRS.NET.PerguntaAi.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DevRS.NET.PerguntaAi;
-using DevRS.NET.PerguntaAi.Controllers;
+using System.Web.Mvc;
 
 namespace DevRS.NET.PerguntaAi.Tests.Controllers
 {
@@ -22,33 +17,7 @@ namespace DevRS.NET.PerguntaAi.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
             Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+        }       
     }
 }
